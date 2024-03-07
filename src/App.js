cc4948937641de870323d13e7edbf1c9aa956c80
Christine Santos <56892403+christinemsantos@users.noreply.github.com>
2024-03-07 12:52:16 -0500

    react app creation & set up, bootstrap added

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Hello
+        <a href="#" className="btn btn-primary">
+          Search
+        </a>
+      </header>
+    </div>
+  );
+}
