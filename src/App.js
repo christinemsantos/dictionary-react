7442da4a14c83580b3f3695e51848c0a1dce8dae
Christine Santos <56892403+christinemsantos@users.noreply.github.com>
2024-04-05 10:26:43 -0400

    added search bar and alert

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
+import logo from "./logo.png";
 import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        Hello
-        <a href="/" className="btn btn-primary">
-          Search
-        </a>
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>Coded by SheCodes</small>
+        </footer>
+      </div>
     </div>
   );
 }
