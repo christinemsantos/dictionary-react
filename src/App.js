e78e4ea45234b2d655e68d6ef90d7bc6ff4ef2ec
Christine Santos <56892403+christinemsantos@users.noreply.github.com>
2024-04-29 09:59:21 -0400

    Added sections css

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <header className="App-header"></header>
+        <header className="App-header">Dictionary</header>
         <main>
-          <Dictionary />
+          <Dictionary defaultKeyword="sunset" />
         </main>
         <footer className="App-footer">
           <small>Coded by SheCodes</small>
